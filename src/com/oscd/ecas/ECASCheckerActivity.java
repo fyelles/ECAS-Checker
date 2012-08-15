da18ffae9c4d0fac599b94eb94855c5c55eef430
Franck Yelles <franck110@gmail.com>
2012-08-14 23:56:16 -0400

    pre release

--- src/com/oscd/ecas/ECASCheckerActivity.java
+++ src/com/oscd/ecas/ECASCheckerActivity.java
@@ -1,5 +1,8 @@
 package com.oscd.ecas;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
@@ -21,7 +24,7 @@ public class ECASCheckerActivity extends Activity {
 	WebView myWebView;
 	ProgressDialog dialog;
 	String URL = "file:///android_asset/main.html";
-	
+
 	SharedPreferences prefs = null;
 
 	@Override
@@ -35,20 +38,59 @@ public class ECASCheckerActivity extends Activity {
 
 	}
 
+	public void SaveJson(String json) {
+		Log.d("JSON", json);
+		if (json.matches("remember.*")) {
+			Log.d("JSON", "I selected remember");
+
+			prefs = PreferenceManager.getDefaultSharedPreferences(this);
+			Editor editor = prefs.edit();
+			
+			editor.putString("identifierType", GetMatch("identifierType", json));
+			editor.putString("identifier", GetMatch("identifier", json));
+			editor.putString("surname", GetMatch("surname", json));
+			
+			editor.putString("dobDay", GetMatch("dobDay", json));
+			editor.putString("dobYear", GetMatch("dobYear", json));
+			editor.putString("dobMonth", GetMatch("dobMonth", json));
+		
+			editor.putString("countryOfBirth", GetMatch("countryOfBirth", json));
+			editor.putBoolean("saved", true);
+			editor.commit();
+
+		}
+	}
+
+	public String GetMatch(String match, String str) {
+		String myreturn = "-1";
+		String Regxp;
+		if (match.equals("countryOfBirth")) {
+			Regxp = match + "=(\\d+)";
+
+		} else {
+			Regxp = match + "=(.*?)\\&";
+		}
+		Log.d("Regxp ", Regxp);
+
+		Pattern patt = Pattern.compile(Regxp);
+		Matcher m = patt.matcher(str);
+		if (m.find()) {
+			myreturn = m.group(1);
+		}
+		return myreturn;
+
+	}
+
 	private WebView initWebView() {
 		myWebView = new WebView(this);
 
 		myWebView.setWebViewClient(new WebViewClient() {
 			public void onPageFinished(WebView view, String url) {
 				super.onPageFinished(view, url);
-				if ( prefs.getBoolean("saved",false)){
-					Log.d("DEBUG SHARED","Value is  set");
+				if (prefs.getBoolean("saved", false)) {
+					// In here we know that the customer has saved
 					
-				}else{
-					Log.d("DEBUG SHARED","Value is NOT  set");
-				}
-				
-				
+					Log.d("DEBUG SHARED", "Value is  set");
 				if (url.toString().equals(URL)) {
 					view.loadUrl("javascript:$('#idNumberLabel').val(\"5198-7383\");");
 					view.loadUrl("javascript:$('#surnameLabel').val(\"Yelles\");");
@@ -58,10 +100,16 @@ public class ECASCheckerActivity extends Activity {
 					view.loadUrl("javascript:$(\"#dobDay option[value='16']\").prop('selected',true);");
 					view.loadUrl("javascript:$(\"#dobYear option[value='1982']\").prop('selected',true);");
 					view.loadUrl("javascript:$(\"#dobMonth option[value='05']\").prop('selected',true);");
-
 					// place of birth
 					view.loadUrl("javascript:$(\"#cobLabel option[value='131']\").prop('selected',true);");
 				}
+				
+				
+				} else {
+					Log.d("DEBUG SHARED", "Value is NOT  set");
+				}
+
+
 			}
 
 		});
@@ -70,6 +118,7 @@ public class ECASCheckerActivity extends Activity {
 		myWebView.getSettings().setSavePassword(false);
 		myWebView.getSettings().setPluginsEnabled(false);
 		myWebView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+		myWebView.addJavascriptInterface(this, "activity");
 
 		// Scale
 		myWebView.setInitialScale(0);
