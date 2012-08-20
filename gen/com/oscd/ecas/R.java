ed59df27f166a7c2b5b78668b45893fc0aac42f2
Franck Yelles <franck110@gmail.com>
2012-08-19 22:30:03 -0400

    Version 1.0

--- gen/com/oscd/ecas/R.java
+++ gen/com/oscd/ecas/R.java
@@ -12,14 +12,22 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int ic_mail=0x7f020001;
     }
     public static final class id {
-        public static final int webview=0x7f050000;
+        public static final int help=0x7f060001;
+        public static final int webview=0x7f060000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
+    public static final class menu {
+        public static final int menu_option=0x7f050000;
+    }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int body=0x7f040002;
+        public static final int help=0x7f040003;
+        public static final int support=0x7f040001;
     }
 }
--- src/com/oscd/ecas/ECASCheckerActivity.java
+++ src/com/oscd/ecas/ECASCheckerActivity.java
@@ -6,18 +6,24 @@ import java.util.regex.Pattern;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.webkit.JsResult;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.preference.PreferenceManager;
 
 public class ECASCheckerActivity extends Activity {
 
@@ -31,11 +37,61 @@ public class ECASCheckerActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(initWebView());
-		prefs = PreferenceManager.getDefaultSharedPreferences(this);
-		Editor editor = prefs.edit();
-		editor.putBoolean("saved", false);
-		editor.commit();
+		Log.d("OnCreate", "On Create initiated");
 
+	}
+	
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		setContentView(initWebView());
+		Log.d("onResume", "onResume initiated");
+
+	}	
+	
+	
+	
+    @Override
+    // Create the menu based on the XML defintion
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_option, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+	        switch (item.getItemId()) {
+            case R.id.help:
+            	SendEmail();
+                break;
+
+	        }
+		return true;
+	  }
+	
+	    private void SendEmail() {
+	        String body = getString(R.string.body);
+
+	        Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+	        emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[] { getString(R.string.support) });
+	        emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, getString(R.string.help));
+	        emailIntent.setType("plain/text");
+	        emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, body);
+	        startActivity(emailIntent);
+	    }
+	 
+	 
+	
+
+	public boolean isOnline() {
+		ConnectivityManager cm =
+				(ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo netInfo = cm.getActiveNetworkInfo();
+		if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+			return true;
+		}
+		return false;
 	}
 
 	public void SaveJson(String json) {
@@ -43,17 +99,19 @@ public class ECASCheckerActivity extends Activity {
 		if (json.matches("remember.*")) {
 			Log.d("JSON", "I selected remember");
 
-			prefs = PreferenceManager.getDefaultSharedPreferences(this);
-			Editor editor = prefs.edit();
 			
+		    Context mContext = getApplicationContext();
+		    prefs = mContext.getSharedPreferences("myAppPrefs", Context.MODE_PRIVATE);
+		    
+			Editor editor = prefs.edit();
 			editor.putString("identifierType", GetMatch("identifierType", json));
 			editor.putString("identifier", GetMatch("identifier", json));
 			editor.putString("surname", GetMatch("surname", json));
-			
+
 			editor.putString("dobDay", GetMatch("dobDay", json));
 			editor.putString("dobYear", GetMatch("dobYear", json));
 			editor.putString("dobMonth", GetMatch("dobMonth", json));
-		
+
 			editor.putString("countryOfBirth", GetMatch("countryOfBirth", json));
 			editor.putBoolean("saved", true);
 			editor.commit();
@@ -70,7 +128,7 @@ public class ECASCheckerActivity extends Activity {
 		} else {
 			Regxp = match + "=(.*?)\\&";
 		}
-		Log.d("Regxp ", Regxp);
+		//Log.d("Regxp ", Regxp);
 
 		Pattern patt = Pattern.compile(Regxp);
 		Matcher m = patt.matcher(str);
@@ -82,34 +140,51 @@ public class ECASCheckerActivity extends Activity {
 	}
 
 	private WebView initWebView() {
+		
+	    Context mContext = getApplicationContext();
+	    prefs = mContext.getSharedPreferences("myAppPrefs", Context.MODE_PRIVATE);
+	    
 		myWebView = new WebView(this);
 
 		myWebView.setWebViewClient(new WebViewClient() {
+			@Override
 			public void onPageFinished(WebView view, String url) {
 				super.onPageFinished(view, url);
-				if (prefs.getBoolean("saved", false)) {
-					// In here we know that the customer has saved
-					
-					Log.d("DEBUG SHARED", "Value is  set");
 				if (url.toString().equals(URL)) {
-					view.loadUrl("javascript:$('#idNumberLabel').val(\"5198-7383\");");
-					view.loadUrl("javascript:$('#surnameLabel').val(\"Yelles\");");
-					view.loadUrl("javascript:$(\"#idTypeLabel option[value='1']\").prop('selected',true);");
 
-					// day of birth
-					view.loadUrl("javascript:$(\"#dobDay option[value='16']\").prop('selected',true);");
-					view.loadUrl("javascript:$(\"#dobYear option[value='1982']\").prop('selected',true);");
-					view.loadUrl("javascript:$(\"#dobMonth option[value='05']\").prop('selected',true);");
-					// place of birth
-					view.loadUrl("javascript:$(\"#cobLabel option[value='131']\").prop('selected',true);");
+					if (prefs.getBoolean("saved", false)) {
+						// In here we know that the user has saved
+
+						Log.d("DEBUG SHARED", "Value is  set");
+
+						view.loadUrl("javascript:$('#surnameLabel').val(\""
+								+ prefs.getString("surname", null) + "\");");
+
+						view.loadUrl("javascript:$('#idNumberLabel').val(\""
+								+ prefs.getString("identifier", null) + "\");");
+						view.loadUrl("javascript:$(\"#idTypeLabel option[value='"
+								+ prefs.getString("identifierType", null)
+								+ "']\").prop('selected',true);");
+
+						// day of birth
+						view.loadUrl("javascript:$(\"#dobDay option[value='"
+								+ prefs.getString("dobDay", null)
+								+ "']\").prop('selected',true);");
+						view.loadUrl("javascript:$(\"#dobYear option[value='"
+								+ prefs.getString("dobYear", null)
+								+ "']\").prop('selected',true);");
+						view.loadUrl("javascript:$(\"#dobMonth option[value='"
+								+ prefs.getString("dobMonth", null)
+								+ "']\").prop('selected',true);");
+						// place of birth
+						view.loadUrl("javascript:$(\"#cobLabel option[value='131']\").prop('selected',true);");
+
+
+
+					} else {
+						Log.d("DEBUG SHARED", "Value is NOT  set");
+					}
 				}
-				
-				
-				} else {
-					Log.d("DEBUG SHARED", "Value is NOT  set");
-				}
-
-
 			}
 
 		});
@@ -123,9 +198,6 @@ public class ECASCheckerActivity extends Activity {
 		// Scale
 		myWebView.setInitialScale(0);
 		myWebView.setWebChromeClient(new MyJavaScriptChromeClient());
-
-		// Define the JS-Java interaction object
-		// mWebView.addJavascriptInterface(new JSImp(), "native_java");
 
 		// load content
 		myWebView.loadUrl(URL);
@@ -149,16 +221,16 @@ public class ECASCheckerActivity extends Activity {
 				final JsResult result) {
 			// handle Alert event, here we are showing AlertDialog
 			new AlertDialog.Builder(ECASCheckerActivity.this)
-					.setTitle("JavaScript Alert !")
-					.setMessage(message)
-					.setPositiveButton(android.R.string.ok,
-							new AlertDialog.OnClickListener() {
-								public void onClick(DialogInterface dialog,
-										int which) {
-									// do your stuff
-									result.confirm();
-								}
-							}).setCancelable(false).create().show();
+			.setTitle("Message !")
+			.setMessage(message)
+			.setPositiveButton(android.R.string.ok,
+					new AlertDialog.OnClickListener() {
+				public void onClick(DialogInterface dialog,
+						int which) {
+					// do your stuff
+					result.confirm();
+				}
+			}).setCancelable(false).create().show();
 			return true;
 		}
 	}
